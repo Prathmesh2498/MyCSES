e967020117bc91fe57e721e12dc811b736086880
Prathmesh2498 <prathu10@gmail.com>
2022-09-15 22:28:30 +0530

    1. New solution

--- solutions/repetitions.cpp
+++ solutions/repetitions.cpp
@@ -5,17 +5,38 @@
 
 using namespace std;
 
+typedef long long ll;
 
-long long N;
+string N;
 
 //g++ -std=c++11 -O2 -Wall
 
 
-int solve(long long N){
+/*
+CRUX: Keep track of the element you are checking for current count.
+      Update answer everytime you find a mismatch between char value for current count and char value of curr iteration.
+      The last answer update is for the edge case where all letters in sequence are the same or the last substring sequence is the largest.
+*/
+
+int solve(string N){
+    ll count=1;
+    ll answer = 1;
+    
+    char curr = N[0];
+    for (int i=1;i<N.length();i++){
+        if (curr == N[i]){
+            count++;
+        } else {
+            curr = N[i];
+            answer = max(answer, count);
+            count = 1;
+        }
+    }
+    answer = max(answer, count);
+    cout << answer;
     
     return 0;
 }
-
 
 
 int main()
@@ -23,7 +44,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    cin >> N;
+    getline(cin, N);
     solve(N);
 
     return 0;
